e7fa87b2cbcf3ee8ed154362ed457226e2fea521
DAMIAN M <12damie@gmail.com>
2020-11-03 13:12:28 +0100

    implement method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,53 @@
 package com.company;
 
+import java.util.List;
+import java.util.Optional;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        StackOperations stackOperations=new Stack();
+        stackOperations.push("1");
+        stackOperations.push("2");
+        stackOperations.push("3");
+
+        List<String> stack=stackOperations.get();
+
+        for(String str:stack){
+            System.out.println(str);
+        }
+
+        System.out.println("========================================");
+        stackOperations.push("1");
+        stackOperations.push("2");
+        stackOperations.push("3");
+        stackOperations.push("4");
+        stackOperations.push("5");
+        stackOperations.push("6");
+
+        Optional<String> item;
+        do{
+            item=stackOperations.pop();
+            System.out.println(item.orElse("Stack is Empty"));
+        }while (item.isPresent());
+
+        System.out.println("========================================");
+
+        stackOperations.push("1");
+        stackOperations.push("2");
+        stackOperations.push("3");
+        stackOperations.push("4");
+        stackOperations.push("5");
+        stackOperations.push("6");
+
+        System.out.println(stackOperations.pop().orElse("Stack is empty"));
+        System.out.println(stackOperations.pop().orElse("Stack is empty"));
+        System.out.println(stackOperations.pop().orElse("Stack is empty"));
+        System.out.println(stackOperations.pop().orElse("Stack is empty"));
+        System.out.println(stackOperations.pop().orElse("Stack is empty"));
+        System.out.println(stackOperations.pop().orElse("Stack is empty"));
+        System.out.println(stackOperations.pop().orElse("Stack is empty"));
+        System.out.println(stackOperations.pop().orElse("Stack is empty"));
+
     }
 }
--- src/com/company/Stack.java
+++ src/com/company/Stack.java
@@ -1,28 +1,38 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
 
 public class Stack implements StackOperations {
     private List<String> stack;
-    private int size;
-    
+
+
     public Stack() {
-        this.size=0;
+        stack=new LinkedList<>();
     }
 
     @Override
     public List<String> get() {
-        return null;
+        List<String> tmp=new LinkedList<>(this.stack);
+        stack.clear();
+        return tmp;
     }
 
     @Override
     public Optional<String> pop() {
+        if(stack.size()>=1){
+            String tmp=stack.get((stack.size()-1));
+            stack.remove(stack.size()-1);
+            return Optional.of(tmp);
+        }
         return Optional.empty();
+
     }
 
     @Override
     public void push(String item) {
-
+        this.stack.add(item);
     }
 }
